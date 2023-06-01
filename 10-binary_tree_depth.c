c1d97ab592975f3a2beb4052a610d4f3f818c7e7
gabumwaingo <gabumwaingo@gmail.com>
2023-06-01 15:37:45 -0700

    Task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures depth of the binary tree
+ * @tree: pointer to the root node
+ * Return: depth of the tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
+}
