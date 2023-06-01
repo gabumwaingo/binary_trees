55b857fcfe67ef99fc40d521e4bb18de79ade767
gabumwaingo <gabumwaingo@gmail.com>
2023-06-01 01:23:44 -0700

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,13 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = NULL;
 
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
 		return (NULL);
 
 	new->n = value;
